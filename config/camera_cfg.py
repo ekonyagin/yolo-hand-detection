55ad1d3b7dc32dc8eb58de173df83ef93314f78d
egorkonyagin <egorkonyagin@yandex.ru>
2020-06-08 12:39:12 +0300

    added version for ximea camera

---
+++ config/camera_cfg.py
@@ -0,0 +1,15 @@
+CAMERA_PARAMS = {
+ 	"offset_X" : 504,
+ 	"offset_Y" : 100,
+ 	"width" : 1024,
+ 	"height" : 1024,
+ 	"exposure" : 11897,
+ 	"gain" : 9,
+ 	"framerate" : 5,
+ 	"auto_wb" : True,
+ 	"img_format" : "XI_RGB24",
+}
+
+PREPROCESSING = {
+ 	"downsample" : 256
+}
